8f9f405ffb27a19be8967fcaf0bf5f22e0428360
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-28 14:30:27 +0200

    auth features e entity

---
+++ lib/src/features/auth/domain/entities/auth_user.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+class AuthUser extends Equatable {
+  final String id;
+  final String email;
+  final String? name;
+  final String? photURL;
+  const AuthUser({
+    required this.id,
+    required this.email,
+    this.name,
+    this.photURL,
+  });
+  //USER EMPTY per uso di debug e/o quando non ho ancora utente authenticato
+  static const AuthUser empty =
+      AuthUser(id: '', email: '', name: '', photURL: '');
+
+  bool get isEmpty => this == AuthUser.empty; //BUSINESS RULE
+
+  @override
+  List<Object?> get props => [id, email, name, photURL];
+
+  @override
+  bool? get stringify => true;
+}
