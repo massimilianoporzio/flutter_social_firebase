402ac51c2aeca2d4014643ef802b610e5f8ddf34
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-28 20:33:28 +0200

    tolto

--- lib/env/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: 'lib/.env')
-abstract class Env {
-  @EnviedField(varName: "FIREBASE_KEY")
-  static const String firebaseKey = _Env.firebaseKey;
-}
