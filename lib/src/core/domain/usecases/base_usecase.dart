da28c9d9af0d4983088359622f07ef7de271835d
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-28 18:38:48 +0200

    base use case and params

---
+++ lib/src/core/domain/usecases/base_usecase.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Type> call(Params params);
+}
+
+abstract class Params extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
