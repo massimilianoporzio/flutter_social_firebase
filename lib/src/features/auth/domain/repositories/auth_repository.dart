3139b444a3711fe7eb6adfe578ad5b5bfd52cdd8
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-28 17:56:01 +0200

    authrepo

---
+++ lib/src/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,8 @@
+import '../entities/auth_user.dart';
+
+abstract interface class AuthRepository {
+  Stream<AuthUser> get authUserStrem;
+  Future<AuthUser> signUp({required String email, required String password});
+  Future<AuthUser> login({required String email, required String password});
+  Future<void> signOut();
+}
