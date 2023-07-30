176c689cfed592181677a8525b344de32238084c
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-07-30 21:19:05 +0200

    sign up cubit da testare

---
+++ lib/src/features/auth/presentation/blocs/sign_up/sign_up_cubit.dart
@@ -0,0 +1,65 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_social_firebase/src/features/auth/domain/usecases/sign_up_usecase.dart';
+import 'package:flutter_social_firebase/src/features/auth/presentation/blocs/email_status.dart';
+import 'package:flutter_social_firebase/src/features/auth/presentation/blocs/form_status.dart';
+import 'package:flutter_social_firebase/src/features/auth/presentation/blocs/password_status.dart';
+
+import '../../../domain/value_objects/email.dart';
+import '../../../domain/value_objects/password.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  //dipende da un caso d'uso
+  final SignUpUsecase _signUpUsecase;
+  SignUpCubit({required SignUpUsecase signupUseCase})
+      : _signUpUsecase = signupUseCase,
+        super(SignUpState.initial());
+  //metodi chiamti quando cambia l'input nella UI
+  void emailChanged(String value) {
+    try {
+      Email email = Email((p0) => p0.value = value);
+      emit(state.copyWith(
+          email: email,
+          emailStatus: EmailStatus
+              .valid)); //VALID perché non sono andato in eccezione nel creare una Email
+    } on ArgumentError {
+      emit(state.copyWith(emailStatus: EmailStatus.invalid));
+    }
+  }
+
+  void passwordChanged(String value) {
+    try {
+      Password password = Password(
+        (p0) => p0.value = value,
+      );
+      emit(state.copyWith(
+          password: password, passwordStatus: PasswordStatus.valid));
+    } on ArgumentError {
+      emit(state.copyWith(passwordStatus: PasswordStatus.invalid));
+    }
+  }
+
+  Future<void> signUp() async {
+    if (!(state.emailStatus == EmailStatus.valid) ||
+        !(state.passwordStatus == PasswordStatus.valid)) {
+      emit(state.copyWith(formStatus: FormStatus.invalid));
+      //rimetto a iniziali lo stato del form (NON di email e pwd che restano invalidi)
+      emit(state.copyWith(formStatus: FormStatus.initial));
+      return; //esco
+    }
+    //tutto validato da qui in poi
+    //inizio il submit
+    emit(state.copyWith(formStatus: FormStatus.submissionInProgress));
+    try {
+      await _signUpUsecase(SignUpParams(
+        email: state.email!,
+        password: state.password!,
+      ));
+      emit(state.copyWith(formStatus: FormStatus.submissionSuccess));
+    } catch (e) {
+      emit(state.copyWith(formStatus: FormStatus.submissionFailure));
+    }
+  }
+}
