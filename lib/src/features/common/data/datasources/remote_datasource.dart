262599eba1dd8b1087f4ca38983ebe0dd70452c4
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-08-17 18:16:25 +0200

    generica interfaccia per remote datasource

---
+++ lib/src/features/common/data/datasources/remote_datasource.dart
@@ -0,0 +1,42 @@
+//mapper tra un mappa string,dynamic a uno specifico oggetto (o lista di oggetti)
+typedef ObjectMapper<T> = T Function(Map<String, dynamic>? data, {String? id});
+
+//interfaccia per un generico "remote" non necessariamente firebase
+abstract class RemoteDataSource {
+  //agg un doc a una collezione
+  Future<String> addDocument({
+    required String collectionPath,
+    required Map<String, dynamic> data,
+  });
+  //Aggiorna un doc dentro un collezione
+  Future<String> updateDocument({
+    required String collectionPath,
+    required String documentId,
+    required Map<String, dynamic> data,
+  });
+  //ritorna una collezione di doc usando il mapper
+  Future<List<T>> getCollection<T>({
+    required String collectionPath,
+    required ObjectMapper<T> objectMapper,
+  });
+  //ritorna una documento che sta dentro una collezione usando il mapper
+  //(ritorna anche null se non trova il doc)
+  Future<T?> getDocument<T>({
+    required String collectionPath,
+    required String documentId,
+    required ObjectMapper<T> objectMapper,
+  });
+  //ritorna uno STREAM di documenti di modo da stare in ascolto per quando
+  //un doc viene aggiornato (o cancellato e quindi anche null)
+  Stream<T?> streamDocument<T>({
+    required String collectionPath,
+    required String documentId,
+    required ObjectMapper<T> objectMapper,
+  });
+  //ritorna uno STREAM di una collezioni di modo da stare in ascolto per quando
+  //una collezione cambia (agg/rimuovi/modifica documento)
+  Stream<List<T>> streamCollection<T>({
+    required String collectionPath,
+    required ObjectMapper<T> objectMapper,
+  });
+}
